2a9e5cfdbf2725496007853ce17ee6dc04a28696
cavslebron111-james <cavslebron111@gmail.com>
2021-04-21 22:06:39 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,52 +1,104 @@
-import React, { Component } from 'react';
-import {Switch,Route} from "react-router-dom";
-import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './components/Navbar';
-import Productlist from './components/Productlist';
-import Details from './components/Details';
-import Cart from './components/Cart';
-import Default from './components/Default';
-import Modal from './components/Modal';
+import React from "react";
+import {Formik,Field,useField,FieldAttributes} from 'formik';
+import {TextField,Button,Checkbox,Radio,FormControlLabel} from '@material-ui/core';
 
 
+type MyRadioProps = { label: string } & FieldAttributes<{}>;
+const MyRadio: React.FC<MyRadioProps> = ({ label, ...props }) => {
+  const [field] = useField<{}>(props);
+  return <FormControlLabel {...field} control={<Radio />} label={label} />;
+};
 
-
-
-class App extends Component {
-  render() {
-    return (
+const App = () => {
+ 
   
-  <React.Fragment>
-     <Navbar></Navbar>
-     <Switch>
+          return (
+           
+       <div>
+  <Formik initialValues={{firstName: "",
+  lastname:"",
+  isTall:false,
+  gender:[],
+  color:""}}
+          onSubmit={(data, {setSubmitting}) => {
+           setSubmitting(true);
+            // make async call
+            console.log('submit',data);
+            setSubmitting(false)
+          }}
+  >
 
-      <Route exact path="/" component={Productlist}/>
-      <Route path="/Details" component={Details}/>
-      
-      <Route path="/cart" component={Cart}/>
-      
-      <Route component={Default}/>
-     
+  
+  {({values,isSubmitting,handleSubmit}) => (
+    <form onSubmit={handleSubmit}>
+    <Field placeholder="firstname"
+          name="firstName"
+           type="input"
+           as={TextField}
+           />
+     <div>
+           <Field placeholder="lastname"
+          name="lastname"
+           type="input"
+           as={TextField}
+           />
+           </div>
+           <div>
+           <Field name="isTall"
+           type="checkbox"
+           as={Checkbox}
+           />
+           </div>
+
+           <div> cookies: </div>
+           <Field name="gender" value="male" type="checkbox" as={Checkbox}/>
+           <Field name="gender" value="female" type="checkbox" as={Checkbox}/>
+           <Field name="gender" value="bakla" type="checkbox" as={Checkbox}/>
+           <Field name="gender" value="tomboy" type="checkbox" as={Checkbox}/>
+          
+           <div> color: </div>
+           <MyRadio name="color" type='radio' value="red" label="red" />
+           <MyRadio name="color" type='radio' value="yellow" label="yellow" />
+           <MyRadio name="color" type='radio' value="green" label="green" />
+           <MyRadio name="color" type='radio' value="blue" label="blue" />
+           
+           
+          
+          
+          
+          
+          
+           <div>
+           <Button type='submit' disabled={isSubmitting}>submit</Button>
+    </div>
+    <pre>{JSON.stringify(values,null,2)}</pre>
+    
+    
+    </form>
 
 
-     </Switch>
-     < Modal />
-     
+  )}
+  
+  
+  
+  
+  
+  </Formik>     
+       
+       </div>     
+               
+               
  
+
+
+
+
+
  
-
- 
-
-
-
-
-
-  </React.Fragment>
     
 
     );
-  }
+  
 }
 
 export default App;
